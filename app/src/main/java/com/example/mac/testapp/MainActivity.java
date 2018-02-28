54b5e36a230d76b4d774dc695102befcab717c45
vijendrap <vijendra@linkites.com>
2018-02-28 17:58:22 +0530

    test

--- app/src/main/java/com/example/mac/testapp/MainActivity.java
+++ app/src/main/java/com/example/mac/testapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //This is tes only
+        //This is tes only dfgfg
     }
 }
